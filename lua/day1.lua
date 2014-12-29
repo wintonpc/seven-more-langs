e6643e5296b30ea7db626ae4f12e66a980342d26
Peter Winton <pwinton@indigobio.com>
2014-12-29 10:15:54 -0500

    initial commit

---
+++ lua/day1.lua
@@ -0,0 +1,43 @@
+require "lunit"
+
+function ends_in_3(num)
+   return num % 10 == 3
+end
+
+function is_prime(n)
+   if n < 2 then return false end
+   for i = 2, n-1 do
+      if n % i == 0 then
+         return false
+      end
+   end
+   return true
+end
+
+module("day1_tests", lunit.testcase, package.seeall)
+
+function test_ends_in_3()
+   assert_false(ends_in_3(0))
+   assert_true(ends_in_3(3))
+   assert_true(ends_in_3(13))
+   assert_false(ends_in_3(34))
+end
+
+function test_is_prime()
+   assert_not_prime(1)
+   assert_prime(2)
+   assert_prime(3)
+   assert_not_prime(4)
+   assert_prime(5)
+   assert_not_prime(6)
+   assert_not_prime(25)
+   assert_prime(29)
+end
+
+function assert_prime(n)
+   assert_true(is_prime(n))
+end
+
+function assert_not_prime(n)
+   assert_false(is_prime(n))
+end
